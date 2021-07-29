5af8005204ad39a2e06346c5ce1ba8a82d808818
VineethEmmanuel <67647805+VineethEmmanuel@users.noreply.github.com>
2021-07-29 17:35:58 +0530

    Add files via upload

---
+++ coronaGraph.py
@@ -0,0 +1,5 @@
+import plotly.express as px
+import pandas as pd
+df = pd.read_csv("D:\Documents\Python Projects\WHJ Projects\Data Visual\data.csv")
+fig = px.line(df,x="country",y = "cases",color = "date",title = "Per Capita Income")
+fig.show
